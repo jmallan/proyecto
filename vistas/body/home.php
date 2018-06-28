87e5d7523441e0489d133c3180dcbe6cd0e14c1e
devlitus <developercarles@gmail.com>
2018-06-28 10:19:29 +0200

    nueva home libreria editor

--- vistas/body/home.php
+++ vistas/body/home.php
@@ -1,2 +1,49 @@
 <?php 
-echo "soy home";
+function showHome($json){
+
+
+	/* Buscar el permiso del usuario para la Home */
+	$permiso = buscarPermiso('Home');
+
+
+	/* Recoger información a mostrar en el home  */
+
+	$datos = json_decode($json,true);
+	$infoDatos = $datos['data'];
+	$infoUsuario = $datos['otro'];
+	$contenidor = $infoDatos['Contenidor'];
+	$fecha = $infoDatos['Fecha'];
+	$usuario = $infoUsuario['Nombre']." ".$infoUsuario['Apellidos'];
+    $seccio = "cuerpo";
+
+    $contenido = "<script>var content = '<?php echo $contenidor; ?>';</script>";
+    $contenido .= "<div id=\"container\" class=\"m-2\">";
+    $contenido .= "<div id=\"contenido\" class=\" container border fondo p-3\">" ;
+    if ( $permiso == 2 ) {
+
+	    $contenido .= "<div class=\"row pt-2\">";
+	    $contenido .= "<div class=\"col-md-6 mb-3\">";
+	    $contenido .= "<span class=\"font-weight-bold\">Pàgina creada per</span> <span class=\"userold\">$usuario</span>";
+	    $contenido .= "</div>";
+	    $contenido .= "<div class=\"col-md-6 mb-3\">";
+	    $contenido .= "<span class=\"font-weight-bold\">Data de creació</span> <span class=\"dateold\">$fecha</span>";
+	    $contenido .= "</div>";
+	    $contenido .= "</div>";
+	    $contenido .= "</div>";
+	    $contenido .= "<div id=\"container\" class=\"p-5\">";
+	    $contenido .= '<button class="editar btn btn-primary float-right" data-seccio="'.$seccio.'">Modificar</button><button class="guardar btn btn-primary" id="'.$seccio.'">Guardar</button>';
+	    $contenido .= "<div class=\"info border p-3\">$contenidor</div>";
+	    $contenido .= "</div>";
+    }
+    else {
+    	$contenido = "<div id=\"container\" class=\"p-5\">";
+        $contenido .= $contenidor;	
+        $contenido .= "</div>";
+    }
+    
+    $contenido .= "</div>";
+    
+	return $contenido;
+}
+
+?>
